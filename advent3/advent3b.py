b833346e322595c5e014b6fb0d4085e67652fb5f
Jeff Harrington <jeff@cedar.com>
2022-12-03 12:22:14 -0500

    Cleanup

--- advent3/advent3b.py
+++ advent3/advent3b.py
@@ -9,13 +9,12 @@ def calculate(lines: list[str]):
     SCORE_KEY = list(string.ascii_lowercase) + list(string.ascii_uppercase)
     score = 0
     i = 0
-    while i < len(lines):
+    for i in range(0, len(lines), 3):
         sack1 = set(lines[i].strip())
         sack2 = set(lines[i + 1].strip())
         sack3 = set(lines[i + 2].strip())
         common = (sack1 & sack2 & sack3).pop()
         score += SCORE_KEY.index(common) + 1
-        i += 3
     return score
 
 
