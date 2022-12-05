4f071d6b51f8af71ac97547bd937e89fa3fa8883
Jeff Harrington <jeff@cedar.com>
2022-12-05 09:18:18 -0500

    Day 5 (with ifs)

---
+++ advent5/advent5a.py
@@ -0,0 +1,39 @@
+#
+# Day 5: Supply Stacks
+# https://adventofcode.com/2022/day/5
+#
+import re
+from collections import defaultdict
+from typing import Dict
+
+CRATE_PATTERN = re.compile(r"(\[\w\]\s)|((   \s))")
+MOVE_PATTERN = re.compile(r"move (\d+) from (\d+) to (\d+)")
+
+
+def calculate(lines: list[str]):
+    stacks: Dict[int, list] = defaultdict(list)
+    for line in lines:
+        if matches := re.findall(CRATE_PATTERN, line):
+            stack_num = 0
+            for match in matches:
+                match letter := match[0].strip().replace("[", "").replace("]", ""):
+                    case "":
+                        pass
+                    case _:
+                        stacks[stack_num].insert(0, letter)
+                stack_num += 1
+        elif move := re.match(MOVE_PATTERN, line):
+            num_crates = int(move[1])
+            source = int(move[2]) - 1
+            dest = int(move[3]) - 1
+            for i in range(num_crates):
+                crate = stacks[source].pop()
+                stacks[dest].append(crate)
+    return "".join([stacks[i][-1] for i in range(len(stacks))])
+
+
+match __name__:
+    case "__main__":
+        with open("input.txt") as f:
+            lines = f.readlines()
+        print(calculate(lines))
---
+++ advent5/test_advent5a.py
@@ -0,0 +1,17 @@
+from advent5a import calculate
+
+test_input = [
+    "    [D]\n",
+    "[N] [C]\n",
+    "[Z] [M] [P]\n",
+    " 1   2   3 \n",
+    "\n",
+    "move 1 from 2 to 1\n",
+    "move 3 from 1 to 3\n",
+    "move 2 from 2 to 1\n",
+    "move 1 from 1 to 2\n",
+]
+
+answer = calculate(test_input)
+expected_answer = "CMZ"
+assert answer == expected_answer, f"Answer ({answer}) != expected ({expected_answer})"
